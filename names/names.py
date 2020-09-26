70b9ea7a1f2770f9c3f0ab1689d9b682d254a502
PeteDram <me@petejames.com>
2020-09-26 11:06:29 -0700

    Names complete

--- names/names.py
+++ names/names.py
@@ -2,27 +2,102 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+    # Insert the given value into the tree
+    def insert(self, value):
+        def fn(value, curr):
+            if value >= curr.value: #if same value, go right per test?
+                #if right child is None
+                if curr.right is None:
+                    curr.right = BSTNode(value)
+                    return
+                else:
+                    fn(value, curr.right)
+            elif value < curr.value:
+                # if left child is None
+                if curr.left is None:
+                    curr.left = BSTNode(value)
+                    return
+                else:
+                    fn(value, curr.left)
+        fn(value, self) # recursive call
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        def fn(value, curr):
+            #check if self.value is target
+            if value == curr.value:
+                #if yes, return True 
+                return True
+            elif value > curr.value:
+                # go right
+                if curr.right is None:
+                    return False
+                else:
+                    return fn(value, curr.right)
+            elif value < curr.value:
+                # go left
+                if curr.left is None:
+                    return False
+                else:
+                    return fn(value, curr.left)
+        return fn(target, self)   
+
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+f = open('names_1.txt', 'r')
+first = f.readline()
+#create first node branch
+name_tree1 = BSTNode(first)
+for name in f: #read each line in the file
+    if name is first: #skip first line
+        pass
+    else:
+        name_tree1.insert(name) #add each line to the tree
+
+f.close()
+
+f = open('names_2.txt', 'r')
+for name in f:
+    #if in the tree, add it to list
+    if name_tree1.contains(name): 
+        duplicates.append(name)
+
+f.close()
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+
+# original code
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# runtime complexity of original code is: O(n^2) Quadratic due to the nested loops
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# What's the best time you can accomplish?  
+# best time 0.13s
+
+# Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
